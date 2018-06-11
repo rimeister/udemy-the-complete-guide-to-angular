51c24f9b085213a3cd417ac2051b9507b3ec02b3
Riley Boyd <contact@rileyboyd.com>
2018-06-11 10:50:06 -0400

    Section 2 assignment 1 complete: Added two new components, WarningAlert and SuccessAlert. Created WarningAlert manually, used CLI to create SuccessAlert

--- assignments/section 2/basics-assignment-1-start/src/app/app.module.ts
+++ assignments/section 2/basics-assignment-1-start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WarningAlert } from './WarningAlert/WarningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlert,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
