31dfd047a82113b3e36f9978a4c5e9b422c7ef30
Riley Boyd <contact@rileyboyd.com>
2018-06-14 11:48:46 -0400

    Section 2, Assignment 2 complete: Haven't looked at the instructor's solution yet, but mine seems to work perfectly

--- assignments/section 2/basics-assignment-2-start/src/app/app.component.ts
+++ assignments/section 2/basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+	userName:string;
+
+	resetUserName() {
+		this.userName = '';
+	}
+
+	testEmpty() {
+		var isEmpty:boolean;
+		if (this.userName !== '') {
+			isEmpty = false;
+		} else {
+			isEmpty = true;
+		}
+		return isEmpty;
+	}
+
 }
