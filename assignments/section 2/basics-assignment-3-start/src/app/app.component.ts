1527d9d9603fc0f7282354814dce6b9b9117e1ba
Riley Boyd <contact@rileyboyd.com>
2018-06-18 12:31:13 -0400

    section 2 assignment 3 complete

--- assignments/section 2/basics-assignment-3-start/src/app/app.component.ts
+++ assignments/section 2/basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+	showText:boolean = true;
+	clicks = [];
+
+	toggleText() {
+
+		this.showText=!this.showText
+
+		var timeStamp = new Date();
+
+		// Get the time and format it
+		var hours = timeStamp.getHours();
+		var minutes = "0" + timeStamp.getMinutes();
+		var seconds = "0" + timeStamp.getSeconds();
+		var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+		this.clicks.push(formattedTime);
+
+	}
+
+	toggleStyle() {
+
+
+		var displayVal;
+		if (this.showText) {
+			displayVal = 'block';
+		} else {
+			displayVal = 'none';
+		}
+
+		return displayVal;
+	}
+
+	setBackgroundColor(index) {
+		var bgColor;
+
+		if (index > 3) {
+			bgColor = 'blue';
+		} else {
+			bgColor = 'white';
+		}
+
+		return bgColor;
+
+	}
+
 }
