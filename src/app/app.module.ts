267acdefd5c970b18340c5e4eb90a6e26d3a23c1
Riley Boyd <contact@rileyboyd.com>
2018-06-19 15:15:15 -0400

    section 3, lecture 24: adding my architecture for the recipe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
