d887f902777adf4003a121d1690921b65ad522f5
Riley Boyd <contact@rileyboyd.com>
2018-06-20 11:35:36 -0400

    Added *ngFor loop to recipe-list component. Added two recipes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,8 @@ import { Recipe } from '../recipe.model'; // Import recipe model
 export class RecipeListComponent implements OnInit {
 
 	recipes:Recipe[] = [// This property holds and array of recipe objects, defined in recipe.model.ts
-		new Recipe('A test recipe', 'This is a test', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg');
+		new Recipe('Pasta', 'A delicious fettucini alfredo.', 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg'),
+		new Recipe('Pizza', 'Rustic, simple pizza with think crust.', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
 	];
 
 	constructor() { }
